ca9791ff935312640fd107c0c66367aa694ce323
sumeyye <sumeyye@DESKTOP-DUD0S0F>
2022-04-04 22:04:53 +0300

    Generic Repository Design Pattern

---
+++ Core/ETicaretAPI.Application/Repositories/IReadRepository.cs
@@ -0,0 +1,27 @@
+﻿using ETicaretAPI.Domain.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Application.Repositories
+{
+    //Bir veritanından sorgu ile data elde edeceksek buna Read işlemleri diyoruz 
+    //Veritabanındaki işlemleri,sorguları  modelledik 
+    public interface IReadRepository<T> : IRepository<T> where T : BaseEntity
+    {
+        // hangi türde isek (T) bana bütün verileri getir
+        IQueryable<T> GetAll();
+
+        //Şarta uygun olan birden fazla veri elde edilsin 
+        IQueryable<T> GetWhere(Expression<Func<T,bool>>method);
+
+        //Şarta uygun olan ilk veriyi getirir 
+        Task<T> GetSingleAsync(Expression<Func<T,bool>>method);
+         
+        //Id ye uygun hangisi ise onu getirecek 
+        Task<T> GetByIdAsync(string id);
+    }
+}
---
+++ Core/ETicaretAPI.Application/Repositories/IWriteRepository.cs
@@ -0,0 +1,26 @@
+﻿using ETicaretAPI.Domain.Entities.Common;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Application.Repositories
+{
+    //İşlemler sonrasında değeri true veya false döneceği için bool verilmiştir
+    public interface IWriteRepository<T>  : IRepository <T> where T : BaseEntity
+    { 
+        //Gelen modeli ekleme
+        Task<bool> AddAsync(T model);
+        // Birden fazla gelen modeli almak için List i ekliyoruz
+        Task<bool> AddRangeAsync(List<T> datas);
+        bool Remove(T model);
+        bool RemoveRange(List<T> datas);
+        // Id verilen veriyi silme 
+        Task<bool> RemoveAsync(string id);
+        bool Update(T model);
+        Task<int> SaveAsync();
+    }
+    
+}
