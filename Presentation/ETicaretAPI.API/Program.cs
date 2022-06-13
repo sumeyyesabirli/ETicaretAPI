92f7f16384590169d9f4804a43199652ef5b45df
sumeyye <sumeyye@DESKTOP-DUD0S0F>
2022-06-13 16:08:57 +0300

    CORS Politikası

--- Presentation/ETicaretAPI.API/Program.cs
+++ Presentation/ETicaretAPI.API/Program.cs
@@ -3,6 +3,9 @@ using ETicaretAPI.Persistence;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddPersistenceServices();
+builder.Services.AddCors(options => options.AddDefaultPolicy(policy =>
+policy.WithOrigins("http://localhost:4200/","https://localhost:4200/").AllowAnyHeader().AllowAnyMethod()
+));
 
 builder.Services.AddControllers();
 
@@ -16,7 +19,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseCors();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
