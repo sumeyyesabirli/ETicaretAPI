83175ba51f706d848131b54c08d75b0717f2aacf
sumeyye <sumeyye@DESKTOP-DUD0S0F>
2022-06-20 20:17:27 +0300

    Fluent Validation

---
+++ Core/ETicaretAPI.Application/Validators/Products/CreateProductValidator.cs
@@ -0,0 +1,40 @@
+﻿using ETicaretAPI.Application.ViewModels.Products;
+using FluentValidation;
+
+namespace ETicaretAPI.Application.Validators.Products
+{
+    public class CreateProductValidator : AbstractValidator<VM_Create_Product>
+    {
+        public CreateProductValidator()
+        {
+            RuleFor(p => p.Name).NotEmpty()
+                .NotEmpty()
+                .NotNull()
+                    .WithMessage("Lütfen ÜRÜN Adını Boş Bırakmayın. ")
+                .MaximumLength(150)
+                .MinimumLength(5)
+                    .WithMessage("Lütfen ürün adını 5 ile 150 karakter arasında giriniz");
+
+            RuleFor(p => p.Stock).NotEmpty()
+                .NotEmpty()
+                .NotNull()
+                    .WithMessage("Lütfen ÜRÜN Adını Boş Bırakmayın. ")
+
+                    //Stoc sayısal bir değer olduğu içn ve büyüklük küçüklük şartları için .MaximumLength yerine .Must u kullanıyoruz 
+
+                    .Must(s => s >= 0)
+                        .WithMessage("Stok bilgisini adam gibi giriniz");
+
+            RuleFor(p => p.Price)
+               .NotEmpty()
+               .NotNull()
+                   .WithMessage("Lütfen fiyat bilgisini boş geçmeyiniz.")
+               .Must(s => s >= 0)
+                   .WithMessage("Fiyat bilgisi negatif olamaz!");
+
+
+
+
+        }
+    }
+}
---
+++ Infrastructure/ETicaretAPI.Infrastructure/Filters/ValidationFilter.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+
+namespace ETicaretAPI.Infrastructure.Filters
+{
+    public class ValidationFilter : IAsyncActionFilter
+
+        //Actiona gelen isteklerde  devreye giren bi filter
+    {
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            if (!context.ModelState.IsValid)
+            {
+                var errors = context.ModelState
+                       .Where(x => x.Value.Errors.Any())
+                       .ToDictionary(e => e.Key, e => e.Value.Errors.Select(e => e.ErrorMessage))
+                       .ToArray();
+
+                context.Result = new BadRequestObjectResult(errors);
+                return;
+            }
+            // filterler sıralı çalıştıkları için next fonksiyonu bir sonraki filteri delege eder
+
+            await next();
+        }
+    }
+}
